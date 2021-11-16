f076657d20f13c3e072ba05b23415a89d6a24d0a
BMasta <bmaster635@gmail.com>
2021-11-16 07:32:00 -0800

    Implemented definitions and examples

--- app/src/main/java/com/android/dynamic_dictionary/DescPagerAdapter.java
+++ app/src/main/java/com/android/dynamic_dictionary/DescPagerAdapter.java
@@ -1,34 +1,36 @@
 package com.android.dynamic_dictionary;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DescPagerAdapter extends RecyclerView.Adapter<DescPagerAdapter.ViewPagerViewHolder> {
-    private List<WordEntryMeaning> meanings = new ArrayList<>();
+    private List<WordEntryMeaning> meanings;
+    private Context parentContext;
 
-    public DescPagerAdapter(List<WordEntryMeaning> meanings) {
+    public DescPagerAdapter(Context context, List<WordEntryMeaning> meanings) {
         this.meanings = meanings;
+        this.parentContext = context;
     }
 
     public class ViewPagerViewHolder extends RecyclerView.ViewHolder {
         private TextView textViewPartOfSpeech;
+        private RecyclerView recyclerViewDefinitions;
 
         public ViewPagerViewHolder(@NonNull View itemView) {
             super(itemView);
 
             textViewPartOfSpeech = itemView.findViewById(R.id.textViewPartOfSpeech);
+            recyclerViewDefinitions = itemView.findViewById(R.id.recycleViewDefinitions);
         }
     }
 
@@ -42,6 +44,8 @@ public class DescPagerAdapter extends RecyclerView.Adapter<DescPagerAdapter.View
     @Override
     public void onBindViewHolder(@NonNull ViewPagerViewHolder holder, int position) {
         holder.textViewPartOfSpeech.setText(meanings.get(position).getPartOfSpeech());
+        holder.recyclerViewDefinitions.setAdapter(new DefinitionsRecycleViewAdapter(meanings.get(position).getDefinitions()));
+        holder.recyclerViewDefinitions.setLayoutManager(new LinearLayoutManager(parentContext));
     }
 
     @Override
